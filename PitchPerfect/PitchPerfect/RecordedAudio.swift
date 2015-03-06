7797c56904eacf8afc019dff8458a89634ebd6f8
JC Smith <jc@superflydevelopment.com>
2015-03-05 21:48:11 -0600

    Committed by Udacity

---
+++ PitchPerfect/PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by JC Smith on 2/18/15.
+//  Copyright (c) 2015 JC Smith. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
